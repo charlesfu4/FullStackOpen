a3ff1af586abe6c030b16b631c8ae21535d46031
Chu-Cheng Fu <ccfu@kth.se>
2020-12-17 01:16:18 +0100

    Exercise 1.10

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -2,14 +2,34 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
 const Title = (props) => <h1>{props.value}</h1>
-
+const Statistic = ({name, value}) => <div>{name}: {value}</div>
 const Button = (props) => (
   <button onClick={props.handleClick}>
     {props.text}
   </button>
 )
 
-const Statistics = ({name, value}) => <div>{name}: {value}</div>
+const Statistics = (props) => {
+  console.log(props.all)
+  if(props.all === 0){
+    return(
+      <div>
+      <Display text={"No feedback given."} />
+      </div>
+    )
+  }
+  else{
+    return(
+      <div>
+      <Statistic name = {"good"} value = {props.good} />
+      <Statistic name = {"netural"} value = {props.neutral} />
+      <Statistic name = {"bad"} value = {props.bad} />
+      <Statistic name = {"avergae"} value = {(props.good+props.bad*(-1))/props.all} />
+      <Statistic name = {"positive"} value = {props.good/props.all*100+' %'} />
+      </div>
+    )
+  }
+}
 const Display = (props) => <div>{props.text}</div>
 
 const App = () => {
@@ -46,7 +66,6 @@ const App = () => {
   }
 
   console.log(clicks.all)
-  if(clicks.all === 0){
     return(
       <div>
       <Title value = {"Give a feedback!"} /> 
@@ -55,27 +74,13 @@ const App = () => {
       <Button handleClick={handleclickbad} text="bad" />
 
       <Title value = {"Statistics"} />  
-      <Display text={"No feedback given."} />
+      <Statistics 
+      all={clicks.all} 
+      good={clicks.good}
+      neutral={clicks.neutral} 
+      bad={clicks.bad} />
       </div>
     )
-  }
-  else{
-    return (
-      <div>
-        <Title value = {"Give a feedback!"} /> 
-        <Button handleClick={handleclickgood} text="good" />
-        <Button handleClick={handleclickneu} text="neutral" />
-        <Button handleClick={handleclickbad} text="bad" />
-
-        <Title value = {"Statistics"} /> 
-        <Statistics name = {"good"} value = {clicks.good} />
-        <Statistics name = {"netural"} value = {clicks.neutral} />
-        <Statistics name = {"bad"} value = {clicks.bad} />
-        <Statistics name = {"avergae"} value = {(clicks.good+clicks.bad*(-1))/clicks.all} />
-        <Statistics name = {"positive"} value = {clicks.good/clicks.all*100+' %'} />
-      </div>
-    )
-  }
 }
 
 ReactDOM.render(<App />, 
