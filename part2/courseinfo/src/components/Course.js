eac08520414a79651333638324530a5ea74068f3
Chu-Cheng Fu <ccfu@kth.se>
2020-12-19 11:44:09 +0100

    Exercise 2.4

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -3,12 +3,16 @@ import Content from './Content'
 import Header from './Header'
 import Total from './Total'
 
-const Course = ({course}) => {
+const Course = ({courses}) => {
     return (
         <div>
+          {courses.map((course) => 
+          <div key={course.id}>
           <Header course={course} />
           <Content course={course} />
           <Total course={course} />
+          </div>
+          )}
         </div>
     ) 
 }
