081a09d2f2183892d0405effb9958f3f6b2b0a77
Chu-Cheng Fu <ccfu@kth.se>
2020-12-27 20:49:43 +0100

    Exercise 3.7-3.8

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const dateformat = require('dateformat')
-const { response } = require('express')
+const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
+
+morgan.token('body', req => JSON.stringify(req.body))
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 
 let persons = [
