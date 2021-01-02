959ec78c32c995b8005c19cec18986d2d67e8ffc
Chu-Cheng Fu <ccfu@kth.se>
2021-01-02 10:12:28 +0100

    Exercise 4.5

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,6 +1,7 @@
 const dummy = () => {
   return 1
 }
+
 const totalLikes = (blogs) => {
   const reducer = (sum, item) => {
     return sum + item.likes
@@ -10,7 +11,23 @@ const totalLikes = (blogs) => {
     blogs.reduce(reducer, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  const reducer = (item1, item2) => {
+    if(item1.likes > item2.likes) return item1
+    else return item2
+  }
+  if(blogs.length === 0) return undefined
+  const favBlogOrg = blogs.reduce(reducer)
+  let favBlog = {}
+  favBlog.title = favBlogOrg.title
+  favBlog.author = favBlogOrg.author
+  favBlog.likes = favBlogOrg.likes
+  console.log(favBlog)
+  return favBlog 
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
