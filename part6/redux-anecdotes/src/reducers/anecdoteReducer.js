0ab3ba6b4bdf84bce8b7d7d37f0bc31b95f20d1b
Chu-Cheng Fu <ccfu@kth.se>
2021-01-19 19:08:29 +0100

    Exercise 6.13-6.14

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,30 +1,7 @@
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
-
-const getId = () => (100000 * Math.random()).toFixed(0)
-
-const asObject = (anecdote) => {
-  return {
-    content: anecdote,
-    id: getId(),
-    votes: 0
-  }
-}
-
-export const createAnecdote = (content) => {
+export const createAnecdote = (data) => {
   return{
     type: 'NEW_OBJ',
-    data: {
-      content,
-      id: getId(),
-      votes: 0
-    }
+    data
   }
 }
 
@@ -35,9 +12,14 @@ export const voteFor = (id) => {
   }
 }
 
-const initialState = anecdotesAtStart.map(asObject)
+export const initializeAnecdotes = (anecdotes) => {
+  return {
+    type: 'INIT_ANECDOTES',
+    data: anecdotes
+  }
+}
 
-const reducer = (state = initialState, action) => {
+const reducer = (state = [], action) => {
   console.log('state now: ', state)
   console.log('action', action)
   switch (action.type) {
@@ -50,8 +32,10 @@ const reducer = (state = initialState, action) => {
       }
       return state.map(anecdote => anecdote.id !== changedAnecdote.id? anecdote:changedAnecdote)
     }
+    case 'INIT_ANECDOTES':
+      return action.data
     case 'NEW_OBJ':
-      return state.concat(action.data) 
+      return [...state, action.data] 
     default:
       return state
   }
