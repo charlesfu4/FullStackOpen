1b2590deee4cd88525631368c32c3b07f696f6f7
Ubuntu <ccfu@kth.se>
2021-03-09 14:46:09 +0100

    feat: Exercise 9.9

--- part9/patientor/index.ts
+++ part9/patientor/index.ts
@@ -1,10 +1,13 @@
 import express from 'express';
+import cors = require('cors');
 const app = express();
 app.use(express.json());
+app.use(cors());
 
-const PORT = 3000;
 
-app.get('/ping', (_req, res) => {
+const PORT = 3001;
+
+app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
 });
