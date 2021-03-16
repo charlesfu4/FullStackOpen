1144592913864637dc25345538045ebcb3919b37
Ubuntu <ccfu@kth.se>
2021-03-16 13:14:45 +0100

    feat: Exercise 9.19

--- part9/patientor/src/types.ts
+++ part9/patientor/src/types.ts
@@ -9,8 +9,46 @@ export interface Diagnose {
   name: string;
   latin?: string;
 }
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Entry {
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
+}
+
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnose['code']>;
+}
+
+interface Discharge {
+  date: string;
+  criteria: string;
+}
+
+interface SickLeave {
+  startDate: string;
+  endDate: string;
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: Discharge;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: SickLeave;
 }
 
 export interface Patient {
@@ -23,7 +61,18 @@ export interface Patient {
   entries: Entry[];
 }
 
+/*
+// Define special omit for unions
+type UnionOmit<T, K extends string | number | symbol> = T extends unknown ? Omit<T, K> : never;
+// Define Entry without the 'id' property
+type EntryWithoutId = UnionOmit<Entry, 'id'>;
+*/
+
 export type NonSensitiveDiagnose = Omit<Diagnose, 'latin'>;
 export type NonSensitivePatient = Omit<Patient, 'ssn'>;
 export type NewPatient = Omit<Patient, 'id'>;
 export type PublicPatient = Omit<Patient, 'ssn' | 'entries'>;
+export type Entry = 
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
