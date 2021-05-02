b53ae126f244b38148bff8c42bcc7b406f9a821f
Chu-Cheng Fu <ccfu@kth.se>
2021-05-02 22:30:47 +0200

    feat: Exercise 8.10

--- part8/library-frontend/src/components/NewBook.js
+++ part8/library-frontend/src/components/NewBook.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react'
+import { useMutation } from '@apollo/client'
+import { ALL_AUTHORS, ALL_BOOKS, CREATE_BOOK } from '../queries'
 
 const NewBook = (props) => {
   const [title, setTitle] = useState('')
@@ -7,15 +9,19 @@ const NewBook = (props) => {
   const [genre, setGenre] = useState('')
   const [genres, setGenres] = useState([])
 
+  const [ createBook ] = useMutation(CREATE_BOOK, {
+    refetchQueries: [ { query: ALL_BOOKS }, { query: ALL_AUTHORS } ], 
+  })
+
   if (!props.show) {
     return null
   }
 
+
   const submit = async (event) => {
     event.preventDefault()
     
-    console.log('add book...')
-
+    createBook({ variables:{ title, published, author, genres }})
     setTitle('')
     setPublished('')
     setAuhtor('')
@@ -50,7 +56,7 @@ const NewBook = (props) => {
           <input
             type='number'
             value={published}
-            onChange={({ target }) => setPublished(target.value)}
+            onChange={({ target }) => setPublished(parseInt(target.value))}
           />
         </div>
         <div>
