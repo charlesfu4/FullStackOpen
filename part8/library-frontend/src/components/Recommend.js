3de8621b9d581018cc292eb62b6b93f764d206fb
Chu-Cheng Fu <ccfu@kth.se>
2021-05-16 00:00:02 +0200

    feat: Exercise 8.21

--- part8/library-frontend/src/components/Recommend.js
+++ part8/library-frontend/src/components/Recommend.js
@@ -1,11 +1,16 @@
-import { useQuery } from '@apollo/client'
+import { useQuery, useLazyQuery } from '@apollo/client'
 import React, { useEffect, useState } from 'react'
 import { ALL_BOOKS, ME } from '../queries'
 
 const Recommend = ({ show }) => {
-  const result = useQuery(ALL_BOOKS) 
   const me = useQuery(ME)
+  const [getBooks, result] = useLazyQuery(ALL_BOOKS) 
   const [books, setBooks] = useState([])
+
+  const filteredBooks = (myGenre) => {
+    getBooks({ variables: { genre: myGenre }})
+  }
+
   useEffect(() => {
     if(result.data) {
       setBooks(result.data.allBooks)
@@ -16,10 +21,12 @@ const Recommend = ({ show }) => {
     return null
   }
   
+  console.log(me.data.me.favoriteGenre)
 
   return (
     <div>
       <h2>Books</h2>
+      <button onClick={() => filteredBooks(me.data.me.favoriteGenre)}>show recommend</button>
 
       <table>
         <tbody>
@@ -34,20 +41,12 @@ const Recommend = ({ show }) => {
               published
             </th>
           </tr>
-          {books.map(a => {
-            if(a.genres.includes(me.data.me.favoriteGenre)){
-              return(
-                <tr key={a.title} align="left">
-                  <td>{a.title}</td>
-                  <td>{a.author.name}</td>
-                  <td>{a.published}</td>
-                </tr>
-              )
-            }
-            else{
-              return null
-            }
-          }
+          {books.map(a => 
+            <tr key={a.title} align="left">
+              <td>{a.title}</td>
+              <td>{a.author.name}</td>
+              <td>{a.published}</td>
+            </tr>
           )}
         </tbody>
       </table>
